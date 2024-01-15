860be16b377adc07ad1f101ab9296d6cb43539c2
sweexordious <chamirachid1@gmail.com>
2024-01-15 12:25:16 +0100

    chore: fix coverage

--- scripts/test_cover.sh
+++ scripts/test_cover.sh
@@ -5,7 +5,8 @@ PKGS=$(go list ./...)
 
 set -e
 echo "mode: atomic" > coverage.txt
-for pkg in "${PKGS[@]}"; do
+# shellcheck disable=SC2068
+for pkg in ${PKGS[@]}; do
     go test -v -timeout 30m -race -test.short -coverprofile=profile.out -covermode=atomic "$pkg"
     if [ -f profile.out ]; then
         tail -n +2 profile.out >> coverage.txt;
